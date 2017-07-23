e4042edbcc8dd31970262e0edb03e723620c97a0
LuiVC <luisavc09@gmail.com>
2017-07-22 20:45:09 -0500

    until here, its running well

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const product = require('/home/luisa/Documentos/Coding/api-rest/models/product')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -19,8 +22,10 @@ res.status(200).send({mesagge:'El producto se ha recibido'})
 })
 
 app.post('/api/product',(req, res) => {
-
+console.log(req.body);
+res.status(404).send({message:'El producto no existe'})
 })
+
 //Actualizaciones
 app.put('/api/product:productID',(req, res) => {
 
@@ -30,6 +35,14 @@ app.delete('/api/product:productID',(req, res) => {
 
 })
 
+
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+  if (err) {
+    return (console.log(`Error al conectar a la base de datos:${err}`))
+  }
+  console.log('Conexion a la base de datos establecida...')
+
 app.listen(port, () => {
  console.log(`API REST corriendo en http://localhost:${port}`)
 })﻿
+})
